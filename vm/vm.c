c4721ccf1b80298ba352f290cf6b2a67ad200005
KillWave <1353207933@qq.com>
2020-08-09 22:47:41 +0800

    new file:   Cello/Cello.h 	new file:   Cello/libCello.a 	new file:   Cello/libCello.so

--- vm/vm.c
+++ vm/vm.c
@@ -7,9 +7,10 @@
 #include <string.h>
 #include <fcntl.h>
 #include <stdbool.h>
-#define int long long // work with 64bit target  
+
+
 int line;             // line number
-int *text,            // text segment
+intptr_t *text,            // text segment
     *old_text,        // for dump text segment
     *stack;           // stack
 char *data;           // data segment
@@ -18,7 +19,7 @@ char *data;           // data segment
 // SP 指针寄存器，永远指向当前的栈顶。注意的是由于栈是位于高地址并向低地址增长的，所以入栈时 SP 的值减小。
 // BP 基址指针。也是用于指向栈的某些位置，在调用函数时会使用到它。
 // AX 通用寄存器，我们的虚拟机中，它用于存放一条指令执行后的结果。
-int *pc, *bp, *sp, ax, cycle; // virtual machine registers
+intptr_t *pc, *bp, *sp, ax, cycle; // virtual machine registers
 
 // instructions
 enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
@@ -26,25 +27,25 @@ enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
        OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
 
 int eval() {
-    int op, *tmp;
+    intptr_t op, *tmp;
     while (true) {
          op = *pc++; // get next operation code
         //指令集
         if (op == IMM)       { ax = *pc++;  }                                     // load immediate value to ax
         else if (op == LC)   {ax = *(char *)ax;}                               // load character to ax, address in ax
-        else if (op == LI)   {ax = *(int *)ax;}                                // load integer to ax, address in ax
+        else if (op == LI)   {ax = *(intptr_t *)ax;}                                // load integer to ax, address in ax
         else if (op == SC)   {ax = *(char *)*sp++ = ax;}                       // save character to address, value in ax, address on stack
-        else if (op == SI)   {*(int *)*sp++ = ax;}                             // save integer to address, value in ax, address on stack
-        else if (op == PUSH) { printf("push.%d\n",ax,*--sp);  *--sp = ax;}                                     // push the value of ax onto the stack
-        else if (op == JMP)  {pc = (int *)*pc;}                                // jump to the address
-        else if (op == JZ)   {pc = ax ? pc + 1 : (int *)*pc;}                   // jump if ax is zero
-        else if (op == JNZ)  {pc = ax ? (int *)*pc : pc + 1;}                   // jump if ax is zero
-        else if (op == CALL) {*--sp = (int)(pc+1); pc = (int *)*pc;}           // call subroutine
+        else if (op == SI)   {*(intptr_t *)*sp++ = ax;}                             // save integer to address, value in ax, address on stack
+        else if (op == PUSH) {*--sp = ax;}                                     // push the value of ax onto the stack
+        else if (op == JMP)  {pc = (intptr_t *)*pc;}                                // jump to the address
+        else if (op == JZ)   {pc = ax ? pc + 1 : (intptr_t *)*pc;}                   // jump if ax is zero
+        else if (op == JNZ)  {pc = ax ? (intptr_t *)*pc : pc + 1;}                   // jump if ax is zero
+        else if (op == CALL) {*--sp = (intptr_t)(pc+1); pc = (intptr_t *)*pc;}           // call subroutine
         //else if (op == RET)  {pc = (int *)*sp++;}                              // return from subroutine;
-        else if (op == ENT)  {*--sp = (int)bp; bp = sp; sp = sp - *pc++;}      // make new stack frame
+        else if (op == ENT)  {*--sp = (intptr_t)bp; bp = sp; sp = sp - *pc++;}      // make new stack frame
         else if (op == ADJ)  {sp = sp + *pc++;}                                // add esp, <size>
-        else if (op == LEV)  {sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;}  // restore call frame and PC
-        else if (op == LEA)  {ax = (int)(bp + *pc++);}                         // load address for arguments.
+        else if (op == LEV)  {sp = bp; bp = (intptr_t *)*sp++; pc = (intptr_t *)*sp++;}  // restore call frame and PC
+        else if (op == LEA)  {ax = (intptr_t)(bp + *pc++);}                         // load address for arguments.
         //操作符
         else if (op == OR)  ax = *sp++ | ax;
         else if (op == XOR) ax = *sp++ ^ ax;
@@ -68,8 +69,8 @@ int eval() {
         else if (op == CLOS) { ax = close(*sp);}
         else if (op == READ) { ax = read(sp[2], (char *)sp[1], *sp); }
         else if (op == PRTF) { tmp = sp + pc[1]; ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]); }
-        else if (op == MALC) { ax = (int)malloc(*sp);}
-        else if (op == MSET) { ax = (int)memset((char *)sp[2], sp[1], *sp);}
+        else if (op == MALC) { ax = (intptr_t)malloc(*sp);}
+        else if (op == MSET) { ax = (intptr_t)memset((char *)sp[2], sp[1], *sp);}
         else if (op == MCMP) { ax = memcmp((char *)sp[2], (char *)sp[1], *sp);}
         else {
             printf("unknown instruction:%d\n", op);
@@ -99,7 +100,7 @@ int initVM(int poolsize){
     memset(data, 0, poolsize);
     memset(stack, 0, poolsize);
     //初始化栈顶位置
-    bp = sp = (int *)((int)stack + poolsize);
+    bp = sp = (intptr_t *)((intptr_t)stack + poolsize);
     ax = 0;
     return 0;
 }
@@ -113,11 +114,10 @@ int execute(int instruction[],int length ){
     return eval();
 }
 
-#undef int // Mac/clang needs this
+
 
 int main(int argc, char *argv[])
 {
-    #define int long long // work with 64bit target
   
     int poolsize = 256 * 1024; // arbitrary size
 
